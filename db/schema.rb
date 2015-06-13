067f8f14aa9b322d9974b0f9f4d8574a5f82b4aa
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-13 15:22:07 -0600

    tweeks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613180950) do
+ActiveRecord::Schema.define(version: 20150613211327) do
 
   create_table "services", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150613180950) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
