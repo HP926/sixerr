2a20d4d0e644ae0d24b76211df8070b738d87543
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-06-13 14:15:53 -0600

    fixed current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  def current_user
-    @current_user ||= User.where(id: session[:user_id]).first
-  end
+  # def current_user
+  #   @current_user ||= User.where(id: session[:user_id]).first
+  # end
   
-  helper_method :current_user
+  # helper_method :current_user
 
 end
